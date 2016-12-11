1bbffaccc3aaefe3443f6e87ab12d212881c02d1
Robert <rll@bu.edu>
2016-12-11 18:05:23 +0000

    Poll title functionality added

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,4 +1,23 @@
 /* global app */
-app.controller("mainController", function($scope){
-            $scope.name = "Robert";
-        })
+app.controller("mainController", function($scope, $http){
+    
+    $scope.formData = {};
+            
+    $http.get('api/polls')
+        .success(function(data) {
+            console.log(data);
+            $scope.polls = data;
+        });
+            
+    $scope.createPoll = function() {
+        $http.post('/api/polls', $scope.formData)
+            .success(function(data) {
+                alert("Success");
+                $scope.formData = {};
+                $scope.polls = data;
+            })
+            .error(function(err){
+                alert(err);
+            })
+        }
+    })
--- server.js
+++ server.js
@@ -2,10 +2,59 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3500;
 var ejs = require('ejs');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/public'));
-app.engine('.html', require('ejs').renderFile);
-app.set('view engine', 'html');
+mongoose.connect('mongodb://MindShaver:bearbear@ds119718.mlab.com:19718/fcc-db');
+
+app.use(express.static(__dirname + '/public')); // Sets our static pages path to public.
+app.use(bodyParser.urlencoded({'extended':'true'})); // Lets us get our form data, and parses nicely
+app.use(bodyParser.json()); // Lets us work with JSON data.
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // Parses application/vnd.api+json
+
+app.engine('.html', require('ejs').renderFile); // Defines our default engine, in this case HTML
+app.set('view engine', 'html'); // Sets our default engine, in this case HTML
+
+var Polls = mongoose.model('poll', { 
+    title: String
+}) // Now all of our polls are modeled after this, and makes it easier to work with.
+
+app.get('/api/polls', function(req, res){   // Reach out to our API and return all the polls
+    Polls.find(function(err, polls) {
+        if(err) throw err;
+        res.json(polls);
+    })
+})
+
+app.post('/api/polls', function(req, res){     // Reaches out to our API and creates polls with a POST method.
+    var title = req.body.text;
+    
+    Polls.create(
+        {
+        "title": title
+        }
+        , function(err, data) {
+                if(err) console.log(err);
+                Polls.find(function(err, results) {
+                    if(err) console.log(err)
+                    res.json(results);
+                })
+            });
+    });
+
+app.delete('api/polls', function(req, res) {
+    Polls.remove({
+        _id : req.params.polls_id
+    }, function(err, polls) {
+        if(err) throw err;
+        
+        Polls.find(function(err, polls){
+            if(err) throw err;
+            
+            res.json(polls);
+        })
+    })
+})
 
 app.get('/', function(req, res) {
     res.sendFile('index.html', {root: __dirname});
